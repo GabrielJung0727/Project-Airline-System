64720d0db351f169f99b9030be0028d36ba51d50
GabrielJung <98318273+GabrielJung0727@users.noreply.github.com>
2025-04-11 10:37:01 +0900

    Add files via upload

---
+++ project_airplane01/project_airplane01Tests/project_airplane01Tests.swift
@@ -0,0 +1,16 @@
+//
+//  project_airplane01Tests.swift
+//  project_airplane01Tests
+//
+//  Created by Gabriel on 2/23/25.
+//
+
+import Testing
+
+struct project_airplane01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
